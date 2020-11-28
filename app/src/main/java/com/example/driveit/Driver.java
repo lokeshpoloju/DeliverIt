90ebb4075d7d4ea593558c908a5821d4768f35fc
lokeshpoloju <58964195+lokeshpoloju@users.noreply.github.com>
2020-11-27 22:59:40 -0500

    Updated Driver and Customer

---
+++ app/src/main/java/com/example/driveit/Driver.java
@@ -0,0 +1,71 @@
+package com.example.driveit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class Driver extends AppCompatActivity {
+
+    EditText etDriver;
+    Button btndrv;
+    TextView txt1;
+    FirebaseFirestore db2;
+    Boolean flag=true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+        etDriver=findViewById(R.id.etDriver);
+        btndrv=findViewById(R.id.btndrv);
+        txt1=findViewById(R.id.txt1);
+        db2=FirebaseFirestore.getInstance();
+        btndrv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDataDrv();
+            }
+        });
+    }
+    public void getDataDrv(){
+        db2.collection("orders")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            String str= "";
+                            for(DocumentSnapshot document:task.getResult()){
+                                Order order=document.toObject(Order.class);
+                                if(etDriver.getText().toString().equals(order.getDriverid())) {
+                                    str += "Hi " + order.getName() +"\n" + "Your tasks are " +order.getOrderid()+"\n" + " scheduled at " + order.getTime();
+                                    flag=false;
+                                }
+                            }
+                            if(flag==true){
+                                str+="No driver Id "+etDriver.getText().toString() ;
+                            }
+                            txt1.setText(str);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),"Error "+e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/driveit/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.driveit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    Button button1;
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+        button1=(Button)findViewById(R.id.button1);
+        button2=(Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToAdmin();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToDriver();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToCustomer();
+            }
+        });
+
+    }
+    public void moveToAdmin(){
+        Intent i=new Intent(MainActivity.this,Admin.class);
+        startActivity(i);
+    }
+    public void moveToDriver(){
+        Intent i1= new Intent(MainActivity.this,Driver.class);
+        startActivity(i1);
+    }
+
+    public void moveToCustomer(){
+        Intent i2=new Intent(MainActivity.this,Customer.class);
+        startActivity(i2);
+    }
+
+
+}
